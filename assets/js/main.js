bc4bed0d579969829c4313b741a46f4a03a27d68
asadbekqulboyev <asadbekqulboyev@gmail.com>
2024-07-08 21:07:00 +0500

    6-page

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,7 +216,7 @@ function moadalShow(btnShow,modal){
   }
   moadalShow($('.characteristic_show'), $('#caracteristic'))
   moadalShow($('.top_content .button'), $('#carta'))
-
+  moadalShow($('.sxema'), $('#sxema'))
 //  toggleSlide
 function toggleSlide(slideBtn, SlideContent){
  slideBtn.click(function(){
